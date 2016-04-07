78c4298a92a8c68c9da12e50d3e5a799ab855128
Daniel Weis <thought_junky@hotmail.com>
2016-04-07 06:24:00 -0700

    Update help.bat

--- WinPE Scripts V1.01/help.bat
+++ WinPE Scripts V1.01/help.bat
@@ -1,13 +1,13 @@
 Echo off
 echo help         Displays this page
-echo formatc      Clean disk 0, quick format, assign letter C
+echo formatc		Clean disk 0, quick format, assign letter C
 echo formatfw4		Clean disks 1-2, quick format, assign letters E-F
 echo formatfw5		Clean disks 2-3, quick format, assign letters E-F
-echo cleanc			  Romove all partition data from disk 0
-echo mapn			    Force attempt to map network drive N:\ for storing log files
-echo bit		    	Open BurnInTest
-echo snap		    	Take screenshots
-echo taskmgr	  	Run the task manager
+echo cleanc		Romove all partition data from disk 0
+echo mapn		Force attempt to map network drive N:\ for storing log files
+echo bit		Open BurnInTest
+echo snap		Take screenshots
+echo taskmgr		Run the task manager
 ::diskpart
 ::dism
 ::ipconfig
