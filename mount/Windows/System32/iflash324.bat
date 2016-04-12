ae373dbcf2de6338b2a03b97b029a54c77aaf6bd
thoughtjunky <DWeis@key.net>
2016-04-12 16:02:20 -0700

    Removed bat files now combined in bupcfg.bat

--- mount/Windows/System32/iflash324.bat
+++
@@ -1,4 +0,0 @@
-cd "%ProgramFiles%\Gen4\iflash32"
-install.cmd
-iflash32.exe" /u R0042.cap
-cd "%windir%\System32"
--- mount/Windows/System32/iflash325.bat
+++
@@ -1,4 +0,0 @@
-cd "%ProgramFiles%\Gen5\iflash32"
-install.cmd
-iflash32.exe /u R03.02.0003.cap updatebackupbios
-cd "%windir%\System32"
