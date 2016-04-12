1a7ee90a13c5a51937bd8bebd2493f8a740cc17c
thoughtjunky <DWeis@key.net>
2016-04-12 09:56:19 -0700

    added bupcfg.bat, modified startnet.bat to use bupcfg.bat instead of syscfg4.bat, syscfg5.bat, iflash324.bat, iflash325.bat

--- mount/Windows/System32/iflash324.bat
+++ mount/Windows/System32/iflash324.bat
@@ -1,3 +1,4 @@
 cd "%ProgramFiles%\Gen4\iflash32"
+install.cmd
 iflash32.exe" /u R0042.cap
 cd "%windir%\System32"
--- mount/Windows/System32/iflash325.bat
+++ mount/Windows/System32/iflash325.bat
@@ -1,3 +1,4 @@
 cd "%ProgramFiles%\Gen5\iflash32"
+install.cmd
 iflash32.exe /u R03.02.0003.cap updatebackupbios
 cd "%windir%\System32"
