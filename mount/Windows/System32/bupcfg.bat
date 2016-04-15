75325217d8b6964269ec61a05f9a4d669382325e
thoughtjunky <DWeis@key.net>
2016-04-15 14:58:55 -0700

    Expanded error messages
    included findstr.exe from Windows 7 64bit
    because it's not included in WinPE 4

--- mount/Windows/System32/bupcfg.bat
+++ mount/Windows/System32/bupcfg.bat
@@ -1,3 +1,4 @@
+@echo off
 :: Usage
 :: bupcfg [desired program] [motherboard generation]
 :: available programs: iflash32, syscfg
@@ -6,6 +7,7 @@
 goto %1
 
 :iflash32
+
 pushd "%ProgramFiles%\Gen%2\iflash32"
 if "%verbose%" == "true" (
 call install.cmd
@@ -19,12 +21,12 @@ if errorlevel 1 (
 	@echo.
 	exit /b 1
 )
-popd
 goto end
 
 
 :syscfg
 
+echo.
 pushd "%ProgramFiles%\Gen%2\syscfg"
 
 if "%verbose%" == "true" (
@@ -35,11 +37,13 @@ call install.cmd nul nul >nul 2>&1
 
 syscfg /bldfs "" >nul 2>&1
 if errorlevel 1 (
-	call colorecho 0c "unable to load system defaults"
+	call colorecho 0c "Unable to load System Defaults"
 	@echo.
 	exit /b 1
 )
-call colorecho 0a "Loaded Defaults"
+	call colorecho 0a "System Defaults Loaded"
+	@echo.
+	@echo.
 
 if "%2" == "4" (
 syscfg.exe /bcs "" "Main" "Quiet Boot" 0
@@ -49,16 +53,19 @@ syscfg.exe /bcs "" "Server Management" "FRB-2 Enable" 0
 syscfg.exe /bcs "" "USB Configuration" "Make USB Devices Non-Bootable" 1
 )
 
-if "%2" == "5" syscfg /prp ON
+if "%2" == "5" syscfg /prp ON >nul 2>&1
 if errorlevel 1 (
 	call colorecho 0c "Failed to set Power Restore Policy"
 	@echo.
 	exit /b 1
 )
+call colorecho 0a "Power Restore Policy set ON"
+	@echo.
+	@echo.
 
 syscfg /bbosys
 
-@echo Type the correct boot sequence i.e. "2134" to change boot order.
+	@echo Type the correct sequence i.e. 2 1 3 4 to change boot order.
 
 set bootorder=blank
 
@@ -71,16 +78,17 @@ if not "%bootorder%" == "blank" (
 )
 
 if errorlevel 1 (
-	call colorecho 0c "Something went wrong setting the boot order"
+	call colorecho 0c "Something went wrong setting Boot Order"
 	@echo.
-	call colorecho 0c "did you put spaces between your numbers?"
-	@echo.
+	call colorecho 0c "did you put spaces between your numbers"
+	@echo ?
 	popd
 	exit /b 1
 )
 
 if not "%bootorder%" == "blank" (
-	call colorecho 0a "Boot order set successfully"
+	call colorecho 0a "Boot Order set"
+	@echo.
 )
 
 :end
--- mount/Windows/System32/startnet.cmd
+++ mount/Windows/System32/startnet.cmd
@@ -1,3 +1,4 @@
+
 @echo OFF
 
 :: Make the variables behave
@@ -24,7 +25,7 @@ call colorecho 0b "Ensure all ports and drives are ready for testing"
 @echo.
 
 :: the only thing that originally existed in this .cmd
-@echo Initializing services...
+@echo Initializing Windows PE Services...
 wpeinit
 @echo.
 
@@ -92,16 +93,26 @@ if "%serialnumber%" == "test" goto cmdline
 powershell set-executionpolicy unrestricted
 
 :: Map the network folder for logging BIT results
-@echo Attempting to map network drive...
-net use N: \\pxeserver\BITPE_LOGS password /user:admin
+	@echo Attempting to map network drive...
+net use N: \\pxeserver\BITPE_LOGS password /user:admin >nul 2>&1
+	@echo.
+	@echo.
+	
 goto setdisk
 
 :setdisk 
 :: Strip and reassign drive letters depending on configuration
-@echo Assigning drive letters and formatting Disk 0 ...
+	@echo Assigning drive letters and formatting Disk 0 ...
+	@echo.
+if errorlevel 1 (
+	call colorecho 06 "Network Share not found"
+) else (
+	call colorecho 0a "Network Share Mapped"
+)
+	@echo.
 diskpart /s volstrip.txt >nul 2>&1
 if "%mobo%" == "S1200RP" (
-		diskpart /s diskpartGen5.txt >nul 2>&1
+	diskpart /s diskpartGen5.txt >nul 2>&1
 ) else (
 	if "%fw%" == "true" diskpart /s diskpartGen4.txt >nul 2>&1
 	if not "%fw%" == "true" diskpart /s diskpartGen4dd.txt >nul 2>&1
@@ -112,60 +123,73 @@ goto setip
 if exist N:\ goto online
 netsh int ipv4 set address ethernet static 192.168.0.1 255.255.255.0 127.0.0.1
 netsh int ipv4 set address "ethernet 2" static 192.168.0.2 255.255.255.0 127.0.0.1
+if errorlevel 1 (
+	call colorecho 0c "Failed to set Static IP Addresses"
+) else (
+	call colorecho 0a "Static IP Addresses Set"
+	goto cmdline
+)
+	@echo.
 goto burnintest
 
 :burnintest
 if "%fw%" == "true" goto FW0
 	call colorecho 0a "Launching BurnInTest G6_SelftestDD script..."
 	@echo.
-"x:\Program Files\BurnInTest\bit.exe" –h -x -r -c G6_SelftestDD.bitcfg
+call "x:\Program Files\BurnInTest\bit.exe" –h -x -r -c G6_SelftestDD.bitcfg"
 goto end
 
 :FW0
 	call colorecho 0a "Launching BurnInTest G6_Selftest script..."
 	@echo.
-"x:\Program Files\BurnInTest\bit.exe" –h -x -r -c G6_Selftest.bitcfg
+call "x:\Program Files\BurnInTest\bit.exe" –h -x -r -c G6_Selftest.bitcfg"
 goto end
 
 :online
 if "%fw%" == "true" goto FW1
 	call colorecho 0a "Launching BurnInTest G6_DD script..."
 	@echo.
-"x:\Program Files\BurnInTest\bit.exe" –h -x -r -c G6_DD.bitcfg
+call "x:\Program Files\BurnInTest\bit.exe" –h -x -r -c G6_DD.bitcfg"
 goto end
 
 :FW1
 	call colorecho 0a "Launching BurnInTest G6 script..."
 	@echo.
-"x:\Program Files\BurnInTest\bit.exe" –h -x -r -c G6.bitcfg
+call "x:\Program Files\BurnInTest\bit.exe" –h -x -r -c G6.bitcfg"
 goto end
 
 :end
 :: If something went wrong, don't shutdown
 if errorlevel 1 (
-	@echo.
-	call colorecho 0c "Looks like BurnInTest crashed"
-	@echo.
-	call colorecho 0c "Try again."
-	@echo.
-	@echo.	
+	call colorecho 0c "Something went wrong"
+) else (
+	call colorecho 0a "BurnInTest Complete"
 	goto cmdline
 )
+	@echo.
+	@echo.
 
 :: Erase all partition data from primary disk
-diskpart /s diskclean.txt
+diskpart /s diskclean.txt >nul 2>&1
+if errorlevel 1 (
+	call colorecho 0c "Failed to Clean Disk 0"
+) else (
+	call colorecho 0a "Disk 0 Cleaned"
+	goto cmdline
+)
+	@echo.
+	@echo.
 
 :: Eject the CD
 powershell eject
 if errorlevel 1 (
-	@echo.
 	call colorecho 0c "Unable to eject CD"
-	@echo.
-	call colorecho 0c "Try again."
-	@echo.
-	@echo.
+) else (
+	call colorecho 0a "CD Ejected"
 	goto cmdline
 )
+	@echo.
+	@echo.
 
 :: Test completed successfully
 	call colorecho 0a "Testing complete, shutting down..."
