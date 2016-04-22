b397144bcd0c404fc74ffb1f311131263f839133
thoughtjunky <DWeis@key.net>
2016-04-22 12:27:40 -0700

    updated help.bat

--- mount/Windows/System32/help.bat
+++ mount/Windows/System32/help.bat
@@ -1,28 +1,48 @@
 @Echo off
+@echo.
 @echo help		Displays this page
-@echo formatc		Clean disk 0, quick format, assign letter C
-@echo formatfw4	Clean disks 1-2, quick format, assign letters E-F
-@echo formatfw5	Clean disks 2-3, quick format, assign letters E-F
-@echo cleanc		Romove all partition data from disk 0
-@echo mapn		Force attempt to map network drive N:\ for storing log files
-@echo g5update	Manually update Gen 5 Firmware
-@echo startnet [v]	Rerun script. v parameter turns Echo ON for troubleshooting
-@echo bit		Open BurnInTest
-@echo taskmgr		Run the task manager
+@echo.
 @echo snap [path]	Take screenshots. path must include full filename 
 @echo			i.e. G:\capture.png
-::diskpart
-::dism
-::ipconfig
-::net
-::netcfg
-::netsh
-::notepad
-::ping
-::regedt32
-::replace
-::robocopy
-::services
-::wdscapture
-::wdsclient
-::wdsmcast
+@echo.
+@echo formatc		Clean disk 0, quick format, assign letter C
+@echo.
+@echo formatfw4	Clean disks 1-2, quick format, assign letters E-F
+@echo.
+@echo formatfw5	Clean disks 2-3, quick format, assign letters E-F
+@echo.
+@echo cleanc		Romove all partition data from disk 0
+@echo.
+@echo mapn		Force attempt to map network drive N:\ for storing log files
+@echo.
+@echo g5update	Manually update Gen 5 Firmware
+@echo.
+@echo startnet [v]	Rerun script. v parameter turns Echo ON for troubleshooting
+@echo.
+@echo bit		Open BurnInTest
+@echo.
+@echo taskmgr		Run the task manager
+@echo.
+@echo diskpart	Windows Disk Partitioning Utility
+@echo.
+@echo bootsect	Manage Master Boot record
+@echo.
+@echo bcdedit		Boot Configuration Data Store Editor
+@echo.
+@echo bcdboot		Bcd boot file creation and repair tool
+@echo.
+@echo dism		Deployment Image Servicing and Management
+@echo.
+@echo ipconfig	Show network information
+@echo.
+@echo notepad		useful for looking at or editing the batch files
+@echo.
+@echo ping		test a network connection
+@echo.
+@echo regedit		edit the registry
+@echo.
+@echo wpeutil		manage services, shutdown or reboot computer
+@echo.
+@echo.
+@echo Powershell Set-CDDriveState [-Eject^|-Close]	manually eject or close the CD tray
+@echo.
